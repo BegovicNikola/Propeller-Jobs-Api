965f2a754e8c831839f32f668a3e8d08046012b4
BegovicNikola <begovic.office@gmail.com>
2020-04-10 01:03:33 +0200

    Async middleware cleanup

--- controllers/company.js
+++ controllers/company.js
@@ -1,110 +1,91 @@
 const Company = require('../models/Company');
 const ErrorResponse = require('../middleware/utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 
 // @desc    Get All Companies
 // @route   GET /api/v1/company
 // @access  Public
-const getCompanies = async (req, res, next) => {
-  try {
-    const companies = await Company.find();
+const getCompanies = asyncHandler(async (req, res, next) => {
+  const companies = await Company.find();
 
-    res.status(201).json({
-      success: true,
-      count: companies.length,
-      data: companies,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(201).json({
+    success: true,
+    count: companies.length,
+    data: companies,
+  });
+});
 
 // @desc    Get Single Comapny
 // @route   GET /api/v1/company/:id
 // @access  Public
-const getCompany = async (req, res, next) => {
-  try {
-    const company = await Company.findById(req.params.id);
+const getCompany = asyncHandler(async (req, res, next) => {
+  const company = await Company.findById(req.params.id);
 
-    if (!company) {
-      throw new ErrorResponse(
-        `Company with the id: ${req.params.id} is not found`,
-        404
-      );
-    }
-
-    res.status(201).json({
-      success: true,
-      data: company,
-    });
-  } catch (err) {
-    next(err);
+  if (!company) {
+    throw new ErrorResponse(
+      `Company with the id: ${req.params.id} is not found`,
+      404
+    );
   }
-};
+
+  res.status(201).json({
+    success: true,
+    data: company,
+  });
+});
 
 // @desc    Create Company
 // @route   POST /api/v1/company
 // @access  Private
-const createCompany = async (req, res, next) => {
-  try {
-    const company = await Company.create(req.body);
+const createCompany = asyncHandler(async (req, res, next) => {
+  const company = await Company.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: company,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: company,
+  });
+});
 
 // @desc    Update Company
 // @route   PUT /api/v1/company/:id
 // @access  Private
-const updateCompany = async (req, res, next) => {
-  try {
-    const company = await Company.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+const updateCompany = asyncHandler(async (req, res, next) => {
+  const company = await Company.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    if (!company) {
-      throw new ErrorResponse(
-        `Company with the id: ${req.params.id} is not found`,
-        404
-      );
-    }
-
-    res.status(200).json({
-      success: true,
-      data: company,
-    });
-  } catch (err) {
-    next(err);
+  if (!company) {
+    throw new ErrorResponse(
+      `Company with the id: ${req.params.id} is not found`,
+      404
+    );
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: company,
+  });
+});
 
 // @desc    Delete Company
 // @route   DELETE /api/v1/company/:id
 // @access  Private
-const deleteCompany = async (req, res, next) => {
-  try {
-    const company = await Company.findByIdAndDelete(req.params.id);
+const deleteCompany = asyncHandler(async (req, res, next) => {
+  const company = await Company.findByIdAndDelete(req.params.id);
 
-    if (!company) {
-      throw new ErrorResponse(
-        `Company with the id: ${req.params.id} is not found`,
-        404
-      );
-    }
-    console.log(company);
-    res.status(200).json({
-      success: true,
-      data: company,
-    });
-  } catch (err) {
-    next(err);
+  if (!company) {
+    throw new ErrorResponse(
+      `Company with the id: ${req.params.id} is not found`,
+      404
+    );
   }
-};
+  console.log(company);
+  res.status(200).json({
+    success: true,
+    data: company,
+  });
+});
 
 module.exports = {
   getCompanies,
