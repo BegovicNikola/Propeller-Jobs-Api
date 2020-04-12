f00eb52e8dd288bb38762fce00e552c1d4525b56
BegovicNikola <begovic.office@gmail.com>
2020-04-12 16:24:41 +0200

    Environment variables fix

--- middleware/utils/geocoder.js
+++ middleware/utils/geocoder.js
@@ -1,9 +1,9 @@
 const NodeGeocoder = require('node-geocoder');
 
 const options = {
-  provider: process.env.GEOCODER_PROVIDER || 'mapquest',
+  provider: process.env.GEOCODER_PROVIDER,
   httpAdapter: 'https',
-  apiKey: process.env.GEOCODER_API_KEY || 'dnp0pjWh58ol2wKADweUPueEGsidGhLz',
+  apiKey: process.env.GEOCODER_API_KEY,
   formatter: null,
 };
 
